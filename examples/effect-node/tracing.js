1c9894b3fc2964d97564f101318e5aa443aac463
Johannes Schickling <schickling.j@gmail.com>
2023-06-30 18:22:23 +0000

    fix effect

--- examples/effect-node/tracing.js
+++ examples/effect-node/tracing.js
@@ -4,13 +4,15 @@ import * as Layer from "@effect/io/Layer"
 import * as NodeSdk from "@effect/opentelemetry/NodeSdk"
 import * as Resource from "@effect/opentelemetry/Resource"
 import * as Tracer from "@effect/opentelemetry/Tracer"
-import { ConsoleSpanExporter } from "@opentelemetry/sdk-trace-base"
+// import { ConsoleSpanExporter } from "@opentelemetry/sdk-trace-base"
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
 
 const ResourceLive = Resource.layer({ serviceName: "effect-node" })
 
 const NodeSdkLive = NodeSdk.layer(Effect.sync(() =>
   NodeSdk.config({
-    traceExporter: new ConsoleSpanExporter()
+    // traceExporter: new ConsoleSpanExporter()
+    traceExporter: new OTLPTraceExporter()
   })
 ))
 
