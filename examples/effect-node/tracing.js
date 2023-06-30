7f05c0ec6a4dde829e9e90cddd57ea558ef5c463
Johannes Schickling <schickling.j@gmail.com>
2023-06-30 18:20:27 +0000

    upgrade deps

---
+++ examples/effect-node/tracing.js
@@ -0,0 +1,34 @@
+import { pipe } from "@effect/data/Function"
+import * as Effect from "@effect/io/Effect"
+import * as Layer from "@effect/io/Layer"
+import * as NodeSdk from "@effect/opentelemetry/NodeSdk"
+import * as Resource from "@effect/opentelemetry/Resource"
+import * as Tracer from "@effect/opentelemetry/Tracer"
+import { ConsoleSpanExporter } from "@opentelemetry/sdk-trace-base"
+
+const ResourceLive = Resource.layer({ serviceName: "effect-node" })
+
+const NodeSdkLive = NodeSdk.layer(Effect.sync(() =>
+  NodeSdk.config({
+    traceExporter: new ConsoleSpanExporter()
+  })
+))
+
+const TracingLive = Layer.provide(
+  ResourceLive,
+  Layer.merge(NodeSdkLive, Tracer.layer)
+)
+
+const program = pipe(
+  Effect.log("Hello"),
+  Effect.withSpan("c"),
+  Effect.withSpan("b"),
+  Effect.withSpan("a")
+)
+
+pipe(
+  program,
+  Effect.provideLayer(TracingLive),
+  Effect.catchAllCause(Effect.logErrorCause),
+  Effect.runFork
+)
